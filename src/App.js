f4e81e654c2ef0281b655a4595fb7c134e2b925b
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-02-04 13:27:49 +0700

    Creacion de componente Items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
+import Items from "./components/Items";
 
 function App() {
 
-  const items = [];
+  const itemsd = [];
 
   return (
     <div className="container">
       <div className="row justify-content-center">
         <Header />
+        <Items />
       </div>
     </div>
   );
---
+++ src/components/Items.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Items = () => {
+  return (
+    <div className="col-sm-10">
+        <div className="row item">
+            <div className="col-12">
+                Nombre
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Edad
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Peso
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Propietario
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Telefono
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Sintomas
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Fecha
+            </div>
+        </div>
+        <div className="row item">
+            <div className="col-12">
+                Hora
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Items;
